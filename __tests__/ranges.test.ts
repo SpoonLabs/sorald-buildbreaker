c94e2084d13d9769ad712f91a54dfc6b6216bbe9
Simon Larsén <slarse@slar.se>
2021-05-03 10:17:58 +0200

    Format ranges.test.ts

--- __tests__/ranges.test.ts
+++ __tests__/ranges.test.ts
@@ -6,7 +6,12 @@ function closedRange(start: number, end: number): ClosedRange {
 
 test('overlapsAny true when identical range is in compare', () => {
   const soughtRange = closedRange(0, 3);
-  const ranges = [closedRange(10, 30), soughtRange, closedRange(4, 10), closedRange(33, 43)];
+  const ranges = [
+    closedRange(10, 30),
+    soughtRange,
+    closedRange(4, 10),
+    closedRange(33, 43)
+  ];
 
   expect(overlapsAny(soughtRange, ranges)).toBe(true);
 });
@@ -14,7 +19,12 @@ test('overlapsAny true when identical range is in compare', () => {
 test('overlapsAny true when sought range ends on compare range start', () => {
   const soughtRange = closedRange(10, 15);
   const barelyOverlaps = closedRange(soughtRange.end, soughtRange.end + 5);
-  const ranges = [closedRange(100, 101), closedRange(93, 204), barelyOverlaps, closedRange(-100, -90)];
+  const ranges = [
+    closedRange(100, 101),
+    closedRange(93, 204),
+    barelyOverlaps,
+    closedRange(-100, -90)
+  ];
 
   expect(overlapsAny(soughtRange, ranges)).toBe(true);
 });
@@ -22,7 +32,12 @@ test('overlapsAny true when sought range ends on compare range start', () => {
 test('overlapsAny true when sought range starts on compare range end', () => {
   const soughtRange = closedRange(10, 15);
   const barelyOverlaps = closedRange(soughtRange.start - 5, soughtRange.start);
-  const ranges = [closedRange(100, 101), closedRange(93, 204), barelyOverlaps, closedRange(-100, -90)];
+  const ranges = [
+    closedRange(100, 101),
+    closedRange(93, 204),
+    barelyOverlaps,
+    closedRange(-100, -90)
+  ];
 
   expect(overlapsAny(soughtRange, ranges)).toBe(true);
 });
@@ -30,15 +45,25 @@ test('overlapsAny true when sought range starts on compare range end', () => {
 test('overlapsAny false when sought range ends on compare range start - 1', () => {
   const soughtRange = closedRange(10, 15);
   const justOutside = closedRange(soughtRange.end + 1, soughtRange.end + 5);
-  const ranges = [closedRange(100, 101), closedRange(93, 204), justOutside, closedRange(-100, -90)];
+  const ranges = [
+    closedRange(100, 101),
+    closedRange(93, 204),
+    justOutside,
+    closedRange(-100, -90)
+  ];
 
   expect(overlapsAny(soughtRange, ranges)).toBe(false);
-})
+});
 
 test('overlapsAny false when sought range starts on compare range end + 1', () => {
   const soughtRange = closedRange(10, 15);
   const justOutside = closedRange(soughtRange.start - 5, soughtRange.start - 1);
-  const ranges = [closedRange(100, 101), closedRange(93, 204), justOutside, closedRange(-100, -90)];
+  const ranges = [
+    closedRange(100, 101),
+    closedRange(93, 204),
+    justOutside,
+    closedRange(-100, -90)
+  ];
 
   expect(overlapsAny(soughtRange, ranges)).toBe(false);
-})
+});
