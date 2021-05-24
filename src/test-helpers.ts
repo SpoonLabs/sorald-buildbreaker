edeaf15641e2d14d6e8314eb6648a7b66c9f9cfc
Simon Larsén <slarse@slar.se>
2021-05-24 13:31:49 +0200

    Add test helper for fetching resources

--- src/test-helpers.ts
+++ src/test-helpers.ts
@@ -1,6 +1,9 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import {PathLike} from 'path';
 import * as os from 'os';
+
+export const RESOURCES_DIR = path.join(__dirname, '../__tests__/data');
 
 export const SORALD_JAR_URL =
   'https://github.com/SpoonLabs/sorald/releases/download/sorald-0.1.0/sorald-0.1.0-jar-with-dependencies.jar';
@@ -18,4 +21,14 @@ export async function createFile(
   const file = await fs.promises.open(filepath, 'w');
   await file.write(content);
   await file.close();
+}
+
+/**
+ * Get a the path to test resource.
+ *
+ * @param resourceName - Name of the test resource file
+ * @returns Absolute path to the test resource
+ */
+export function getResourcePath(resourceName: PathLike): PathLike {
+  return path.join(RESOURCES_DIR, resourceName.toString());
 }
